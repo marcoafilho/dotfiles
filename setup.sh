c7b83d390f79bd9b1eb981102210444df33ecbc4
Marco Antonio Almeida <marco.almeida@karnovgroup.com>
2017-02-16 20:32:10 +0100

    Use a better way to find the script's root folder.

--- setup.sh
+++ setup.sh
@@ -1,1 +1,9 @@
-ln -fs ~/Projects/dotfiles/git/gitconfig ~/.gitconfig
+#!/bin/bash
+
+# Set the current path into SCRIPT_PATH
+pushd `dirname $0` > /dev/null
+SCRIPT_PATH=`pwd`
+popd > /dev/null
+
+# Create symbolic link for gitconfig.
+ln -fs $SCRIPT_PATH/git/gitconfig ~/.gitconfig
