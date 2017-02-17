e512c986e6d994d05a0f63e0559f3a5def8dbc4b
Marco Antonio Almeida <marco.almeida@karnovgroup.com>
2017-02-17 19:15:18 +0100

    Set atom configuration

--- setup.sh
+++ setup.sh
@@ -1,9 +1,31 @@
 #!/bin/bash
 
-# Set the current path into SCRIPT_PATH
+# Set the dotfiles directory in order to run this script from anywhere.
 pushd `dirname $0` > /dev/null
-SCRIPT_PATH=`pwd`
+DOTFILES_DIR=`pwd`
 popd > /dev/null
+DOTFILES_BACKUP_DIR=~/dotfiles_old
 
-# Create symbolic link for gitconfig.
-ln -fs $SCRIPT_PATH/git/gitconfig ~/.gitconfig
+declare -a FILES_TO_SYMLINK=(
+  'atom'
+  'git/gitconfig'
+)
+
+# Backup existing dotfiles into the DOTFILES_BACKUP_DIR directory.
+echo "Backing up existing dotfiles from ~ to $DOTFILES_BACKUP_DIR"
+mkdir -p $DOTFILES_BACKUP_DIR
+for i in ${FILES_TO_SYMLINK[@]}; do
+  mv -f ~/.${i##*/} $DOTFILES_BACKUP_DIR
+done
+
+main() {
+  # Create symbolic links.
+  for i in ${FILES_TO_SYMLINK[@]}; do
+    source="$DOTFILES_DIR/$i"
+    target="$HOME/.$(printf "%s" "$i" | sed "s/.*\/\(.*\)/\1/g")"
+
+    ln -fs $source $target
+  done
+}
+
+main
